5c89b27ac589da351e855bb23411cb0ab01ff9c1
sysrecall <uprojectemail@protonmail.com>
2025-05-09 12:08:44 +0600

    Initial Template

---
+++ DAL/BookStoreContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using BookStore.Models;
+
+namespace BookStore.DAL
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext() : base("BookStoreContext")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ DAL/BookStoreInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BookStore.Models;
+using System.Data.Entity;
+
+namespace BookStore.DAL
+{
+    public class BookStoreInitializer : DropCreateDatabaseIfModelChanges<BookStoreContext>
+    {
+        protected override void Seed(BookStoreContext context)
+        {
+            // base.Seed(context);
+            var users = new List<User>
+            {
+                new User{Username="johndoe", PasswordHash="123"}
+            };
+
+            users.ForEach(u => context.Users.Add(u));
+            context.SaveChanges();
+        }
+    }
+}
