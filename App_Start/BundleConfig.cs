04f3f8c2e6f0b2cf1020e2ff64c245ac0a5f4a8e
sysrecall <uprojectemail@protonmail.com>
2025-06-18 16:48:34 +0600

    BugFix: Read Book

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -26,9 +26,7 @@ namespace BookStore
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
-            
-            bundles.Add(new StyleBundle("~/Content/pdfjs").Include(
-            "~/Scripts/pdfjs/web/viewer.css"));
+
         }
     }
 }
